ce74d26dbdbfd4e5f669d191b09a6d43d931b512
unknown <manvirm29@gmail.com>
2022-01-03 21:12:20 -0800

    add game logic

--- scraping_project.py
+++ scraping_project.py
@@ -2,6 +2,7 @@
 # In this program we scrape http://quotes.toscrape.com for quotes 
 
 import requests
+import random
 from bs4 import BeautifulSoup
 from time import sleep
 
@@ -13,12 +14,13 @@ all_quotes=[]
 #to retrieve new quotes, until we have all quotes
 while url:
 
+    #parse html with beautifulsoup
     response = requests.get(f"{base_url}{url}")
     response.encoding = 'utf-8'
     print(f"Now scraping {base_url}{url}...")
     soup = BeautifulSoup(response.text, "html.parser")
 
-    #extract html with class quote
+    #find all html with class quote
     quotes = soup.find_all(class_="quote")
 
     #loop through all quotes and find class with text, which gives text of quote
@@ -42,5 +44,29 @@ while url:
     #take time in between scraping each page, wait 2 seconds before requesting again to avoid overloading the server
     sleep(2)
 
-print(all_quotes)
+#print(all_quotes)
+
+#game logic
+num = random.randrange(0, len(all_quotes)-1)
+quote = all_quotes[num].get('text')
+author = all_quotes[num].get('author')
+
+answer = input(f"Guess the author of this quote {quote} (4 attempts): ")
+attempts = 1
+
+while answer != author and attempts < 4:
+
+    if(attempts < 3):
+        answer = input(f"Guess the author of this quote {quote} ({4-attempts} attempts remaining): ")
+    else:
+        answer = input(f"Guess the author of this quote {quote} ({4-attempts} attempt remaining): ")
+    attempts += 1
+
+if(attempts == 4):
+    print(f"Good try! The author was {author}")
+else:
+    print(f"Congratulations! You are correct!")
+
+
+
     
