8065061c2093f2457b4cebe7d8f84d9e37917d51
unknown <manvirm29@gmail.com>
2022-01-04 16:24:08 -0800

    add hint function

--- scraping_project.py
+++ scraping_project.py
@@ -9,8 +9,7 @@ from time import sleep
 base_url = "http://quotes.toscrape.com"
 
 
-#--------------- RETRIEVE QUOTES, AUTHORS, AND AUTHOR PAGE LINKS ---------------
-
+#retrieve quotes, the authors for the quotes, and authors about pages
 def scrape_quotes():
 
     all_quotes=[]
@@ -51,8 +50,24 @@ def scrape_quotes():
         
     return all_quotes
 
+#print hint after each incorrect guess
+def print_hint(attempts, author, about):
+    #find author birthday and birthplace and display it as hint
+    if(attempts == 3):
+        res = requests.get(f"{base_url}{about}")
+        soup = BeautifulSoup(res.text, "html.parser")
+        birth_date = soup.find(class_="author-born-date").get_text()
+        birth_location = soup.find(class_="author-born-location").get_text()
+        print(f"Hint #1: This author was born on {birth_date} {birth_location}")
 
-#--------------- GAME LOGIC ---------------
+    #display first name initial as hint
+    elif(attempts == 2):
+        print(f"Hint #2: The author's first name starts with {author[0]}")
+
+    #display last name initial as hint (since name is in the form Manvir Mann, we can use split)
+    elif(attempts == 1):
+        last_initial = author.split(" ")[1][0]
+        print(f"Hint #3: The author's last name starts with {last_initial}")
 
 def start_game(all_quotes):
 
@@ -68,28 +83,13 @@ def start_game(all_quotes):
 
     while answer.lower() != author.lower() and attempts > 0:
 
-        #find author birthday and birthplace and display it as hint
-        if(attempts == 3):
-            res = requests.get(f"{base_url}{about}")
-            soup = BeautifulSoup(res.text, "html.parser")
-            birth_date = soup.find(class_="author-born-date").get_text()
-            birth_location = soup.find(class_="author-born-location").get_text()
-            print(f"Hint #1: This author was born on {birth_date} {birth_location}")
-
-        #display first name initial as hint
-        elif(attempts == 2):
-            print(f"Hint #2: The author's first name starts with {author[0]}")
-
-        #display last name initial as hint (since name is in the form Manvir Mann, we can use split)
-        elif(attempts == 1):
-            last_initial = author.split(" ")[1][0]
-            print(f"Hint #3: The author's last name starts with {last_initial}")
+        print_hint(attempts, author, about)
         
         #promp question to user
         answer = input(f"Try again. Guess the author of this quote {quote} (Attempts remaining: {attempts}):\n")
         attempts -= 1
         
-
+    #check if user answered correct or not
     if(answer.lower() == author.lower()):
         print(f"Congratulations! You are correct! (Answer: {author})")    
     else:
